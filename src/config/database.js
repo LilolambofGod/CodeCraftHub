1e1689041d61ac82ce5c88a528ef49e11b5aa311
LilolambofGod <amanda.zheng1312@gmail.com>
2024-09-14 16:15:33 -0400

    MongoDB url updated

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://root:ewtLUMVUjpnXp08G1OKJxqHl@localhost:27017');
+    await mongoose.connect('mongodb://root:ewtLUMVUjpnXp08G1OKJxqHl@172.21.236.7:27017');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
